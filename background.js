91b40c4e12d942d1cdc2f76dce1f710f884135ae
Annika <annika.xu19@gmail.com>
2024-08-16 15:25:30 -0400

    fixed backend and improved styling again

--- background.js
+++ background.js
@@ -1,100 +1,108 @@
-chrome.runtime.onMessage.addListener(function(request) {
-    //listen for incoming messages to execute provided callback function 
+chrome.runtime.onMessage.addListener(function (request) {
+  //listen for incoming messages to execute provided callback function
 
-    if (request.activateBackgroundJS) {
-        console.log("extension on");
-        //if request is to activate background, store local data that the extension is on
-        chrome.storage.local.set({ extOn: true }).then(() => {
-            console.log("Value is set to " + true);
-          });
-    } else if (request.activateBackgroundJS == false) {
-        console.log("extension off");
-        //if request is to deactivate background, store local data that the extension is off
-        chrome.storage.local.set({ extOn: false }).then(() => {
-            console.log("Value is set to " + false);
-          });
-    }
+  if (request.activateBackgroundJS) {
+    console.log("extension on");
+    //if request is to activate background, store local data that the extension is on
+    chrome.storage.local.set({ extOn: true }).then(() => {
+      console.log("Value is set to " + true);
+    });
+  } else if (request.activateBackgroundJS == false) {
+    console.log("extension off");
+    //if request is to deactivate background, store local data that the extension is off
+    chrome.storage.local.set({ extOn: false }).then(() => {
+      console.log("Value is set to " + false);
+    });
+  }
 
-    //if request is to add a new entry to the list of blocked sites, store list with new entry in local storage
-    if (request.newEntry){
-        console.log("enter clicked")
-        //blocklist.push(request.newEntry)
-        //chrome.storage.local.set({entryText: blocklist})
-        chrome.storage.local.set({entryText: request.newEntry})
-        console.log(request.newEntry)
-    }
+  //if request is to add a new entry to the list of blocked sites, store list with new entry in local storage
+  if (request.newEntry) {
+    console.log("enter clicked");
+    //blocklist.push(request.newEntry)
+    //chrome.storage.local.set({entryText: blocklist})
+    chrome.storage.local.set({ entryText: request.newEntry });
+    console.log(request.newEntry);
+  }
 
-    //if request is to delete an entry from list of blocked sites
-    if (request.deleteEntry){
-        console.log("entry deleted")
+  //if request is to delete an entry from list of blocked sites
+  if (request.deleteEntry) {
+    console.log(chrome.storage.local.get(["entryText"]));
+    //get current blocklist from local storage
+    chrome.storage.local.get(["entryText"], (result) => {
+      console.log("here");
 
-        //get current blocklist from local storage
-        chrome.storage.local.get(["entryText"], (result) => {
-            //get index of blocklist item that is to be deleted
-            const delIndex = result.entryText.indexOf(request.deleteEntry)
-            
-            if (delIndex > -1) { // only splice array when item is found
-                //splice array and store new list in storage
-                result.entryText.splice(delIndex, 1);
-                chrome.storage.local.set({entryText: result.entryText})
-                console.log(result.entryText)
-            }
-        });
-    }
-    
+      console.log("id to delete", request.deleteEntry);
+      console.log("arr", result.entryText);
+      //   console.log("id", result.entryText[1].id);
+      //get index of blocklist item that is to be deleted
+      const delIndex = result.entryText.findIndex(
+        (obj) => obj.id === parseInt(request.deleteEntry)
+      );
+
+      console.log("delindex", delIndex);
+
+      if (delIndex > -1) {
+        // only splice array when item is found
+        //splice array and store new list in storage
+        result.entryText.splice(delIndex, 1);
+        console.log("entry deleted");
+        chrome.storage.local.set({ entryText: result.entryText });
+        console.log(result.entryText);
+      }
+    });
+  }
 });
-
 
 //On switching to a different tab
 chrome.tabs.onActivated.addListener(() => {
-    setTimeout(() => {
-        //Check to see if extension is on
-        chrome.storage.local.get(["extOn"], (result) =>{
-            console.log("Value currently is " + result.extOn);
+  setTimeout(() => {
+    //Check to see if extension is on
+    chrome.storage.local.get(["extOn"], (result) => {
+      console.log("Value currently is " + result.extOn);
 
-            //Get list of sites to be blocked
-            chrome.storage.local.get(["entryText"], (result2) =>{
-                console.log(result2.entryText) 
+      if (result.extOn == true) {
+        //Get list of sites to be blocked
+        chrome.storage.local.get(["entryText"], (result2) => {
+          console.log(result2.entryText);
 
-                //If extension on
-                if (result.extOn==true){
-                    chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
-                        var url = tabs[0].url;
-                        
-                        //check through blocklist and redirect user 
-                        blockSite(url,result2.entryText, url.id)
-                        
-                    });
-                }
-            }); 
+          //If extension on
+
+          chrome.tabs.query(
+            { active: true, lastFocusedWindow: true, currentWindow: true },
+            function (tabs) {
+              var url = tabs[0].url;
+
+              //check through blocklist and redirect user
+              blockSite(url, result2.entryText, url.id);
+            }
+          );
         });
-    }, 100);
-    
+      }
+    });
+  }, 100);
 });
 
 //On entering a new URL
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  //Check to see if extension is on
+  chrome.storage.local.get(["extOn"], (result) => {
+    console.log("Value currently is " + result.extOn);
+    console.log("update", tab.url);
+    //If extension on
+    if (result.extOn == true) {
+      //Get list of sites to be blocked
+      chrome.storage.local.get(["entryText"], (result2) => {
+        console.log("result 2", result2.entryText);
 
-    //Check to see if extension is on
-    chrome.storage.local.get(["extOn"], (result) =>{
-        console.log("Value currently is " + result.extOn);
-        console.log("update",tab.url)
-
-        //Get list of sites to be blocked
-        chrome.storage.local.get(["entryText"], (result2) =>{
-            console.log(result2.entryText)
-
-            //If extension on
-            if (result.extOn==true){
-                //check through blocklist and redirect user 
-                blockSite(tab.url,result2.entryText, tabId)
-            }
-        });
-    });
+        //check through blocklist and redirect user
+        blockSite(tab.url, result2.entryText, tabId);
+      });
+    }
+  });
 });
 
-function blockSite(url,blocklist, tabId){
-    /*Checks through blocklist and redirects user
+function blockSite(url, blocklist, tabId) {
+  /*Checks through blocklist and redirects user
     Parameters:
         url: string 
         blocklist: string list
@@ -102,18 +110,21 @@ function blockSite(url,blocklist, tabId){
     Returns:
         None 
     */
-    for (var i = 0; i < blocklist.length; i++){
-        //check if each site from blocklist is contained within the url 
-        if (url.includes(blocklist[i])){
-            
-            //prevents redirect loop by ending the redirect once the url has already changed 
-            var redirect = chrome.runtime.getURL("redirect.html") + '?url=' + encodeURIComponent(url)
-            
-            if (!url.includes("redirect.html?url=chrome-extension")){
-                chrome.tabs.update(tabId, {url: redirect});
-                redirect = "";
-            }
-        }
+  if (url) {
+    for (var i = 0; i < blocklist.length; i++) {
+      //check if each site from blocklist is contained within the url
+      if (url.includes(blocklist[i].url)) {
+        //prevents redirect loop by ending the redirect once the url has already changed
+        var redirect =
+          chrome.runtime.getURL("redirect.html") +
+          "?url=" +
+          encodeURIComponent(url);
 
+        if (!url.includes("redirect.html?url=chrome-extension")) {
+          chrome.tabs.update(tabId, { url: redirect });
+          redirect = "";
+        }
+      }
     }
+  }
 }
--- popup.js
+++ popup.js
@@ -1,104 +1,114 @@
 //Event DOMContentLoaded fires when initial HTML document has been completely loaded and parsed
-document.addEventListener("DOMContentLoaded",
-    function(){
-    //Get references to elements on HTML (extension on/off toggle switch, blocklist site input form, form submit button)
-    var checkToggleswitch = document.getElementById('extonoff')
-    var textinpt = document.getElementById('siteInpt')
-    var inptButton = document.getElementById('inptButton')
-    var blockButton = document.getElementById("blockthis");
-    
-    //Get reference to button container div
-    var buttonContainer = document.getElementById("buttonContainer");
-    
-    //check local storage to see if extension was last turned on or off
-    chrome.storage.local.get(["extOn"], (result) => {
-        if (result.extOn==true){
-            //if extension is on, show toggle switch as "on" (checked) in popup
-            document.getElementById("extonoff").checked = true;
+document.addEventListener("DOMContentLoaded", function () {
+  //Get references to elements on HTML (extension on/off toggle switch, blocklist site input form, form submit button)
+  var checkToggleswitch = document.getElementById("extonoff");
+  var textinpt = document.getElementById("siteInpt");
+  var inptButton = document.getElementById("inptButton");
+  var blockButton = document.getElementById("blockthis");
+
+  //Get reference to button container div
+  var buttonContainer = document.getElementById("buttonContainer");
+
+  //check local storage to see if extension was last turned on or off
+  chrome.storage.local.get(["extOn"], (result) => {
+    if (result.extOn == true) {
+      //if extension is on, show toggle switch as "on" (checked) in popup
+      document.getElementById("extonoff").checked = true;
+    }
+  });
+
+  //get current list of blocked sites from local storage and creates blocklist if not already existing
+  chrome.storage.local.get(["entryText"], (result) => {
+    if (!result.entryText) {
+      blocklist = [];
+      chrome.storage.local.set({ entryText: blocklist });
+    }
+    blocklist = result.entryText;
+
+    //Generate buttons with names of blocked sites
+    generateButtons();
+  });
+
+  //check for change in toggle switch state, if on, send message to activate background script, if not, don't
+  checkToggleswitch.addEventListener("change", function () {
+    if (checkToggleswitch.checked) {
+      chrome.runtime.sendMessage({ activateBackgroundJS: true });
+    } else {
+      chrome.runtime.sendMessage({ activateBackgroundJS: false });
+    }
+  });
+
+  //check for submit button click (submitting new site url to be blocked)
+  inptButton.addEventListener("click", function () {
+    chrome.storage.local.get(["entryText"], (result) => {
+      blocklist = result.entryText;
+      //add new entry to list of blocked sites if not already blocked
+      if (!blocklist.some((obj) => obj.url == textinpt.value)) {
+        blocklist.push({ url: textinpt.value, id: generateUniqueId() });
+        chrome.runtime.sendMessage({ newEntry: blocklist });
+      }
+    });
+    //chrome.runtime.sendMessage({newEntry: textinpt.value})
+  });
+
+  //Block this button adds current url to blocklist
+  blockButton.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      var url = tabs[0].url;
+
+      chrome.storage.local.get(["entryText"], (result) => {
+        blocklist = result.entryText;
+
+        // check that not already blocked
+        if (!blocklist.some((obj) => obj.url == url)) {
+          //add new entry to list of blocked sites
+          blocklist.push({ url: url, id: generateUniqueId() });
+          chrome.runtime.sendMessage({ newEntry: blocklist });
+          generateButtons();
         }
+      });
     });
+  });
 
-     //get current list of blocked sites from local storage and creates blocklist if not already existing 
-     chrome.storage.local.get(["entryText"], (result) => {
-        if (!result.entryText){
-            blocklist=[]
-            chrome.storage.local.set({"entryText":blocklist})
-        }
-        blocklist = result.entryText
-        
-        //Generate buttons with names of blocked sites
-        generateButtons()
-    });
+  function generateUniqueId() {
+    const timestamp = Date.now(); // Current time in milliseconds
+    const randomNum = Math.floor(Math.random() * 100000); // Random number between 0 and 99999
+    return timestamp + randomNum;
+  }
 
-    //check for change in toggle switch state, if on, send message to activate background script, if not, don't
-    checkToggleswitch.addEventListener('change',function(){
-        if (checkToggleswitch.checked){
-            chrome.runtime.sendMessage({activateBackgroundJS: true});
-        }else{
-            chrome.runtime.sendMessage({activateBackgroundJS: false});
-        }
-    });
-    
-    //check for submit button click (submitting new site url to be blocked)
-    inptButton.addEventListener('click',function(){
-        chrome.storage.local.get(["entryText"], (result) => {
-            blocklist = result.entryText
-            //add new entry to list of blocked sites
-            if(!blocklist.includes(textinpt.value)){
-                blocklist.push(textinpt.value)
-                chrome.runtime.sendMessage({newEntry: blocklist});
-            }  
-        });
-        //chrome.runtime.sendMessage({newEntry: textinpt.value})
-        
-    });
-
-    //Block this button adds current url to blocklist 
-    blockButton.addEventListener('click', function(){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-            var url = tabs[0].url
-            if (!blocklist.includes(url)){
-                chrome.storage.local.get(["entryText"], (result) => {
-                    blocklist = result.entryText
-                    //add new entry to list of blocked sites
-                    blocklist.push(url)
-                    chrome.runtime.sendMessage({newEntry: blocklist});
-                    generateButtons()
-                });
-            }
-        })
-
-    })
-
-    function generateButtons(){
-        /*
+  function generateButtons() {
+    /*
         Generates buttons based on blocklist 
         Parameters: None
         Returns: None 
         */
-        buttonContainer.innerHTML = "";
-        for (var i = 0; i < blocklist.length; i++) {
-            //Create new button element
-            var button = document.createElement("button");
 
-            //Set the button's text
-            button.innerHTML = blocklist[i];
-            button.className = "button";
-            button.onclick = function() {
-                return function() {
-                    //on click, send message to background.js to delete site from blocklist + remove button interface from HTML container
-                    chrome.runtime.sendMessage({deleteEntry:this.innerHTML});
-                    buttonContainer.removeChild(this);
+    buttonContainer.innerHTML = "";
+    if (blocklist) {
+      for (var i = 0; i < blocklist.length; i++) {
+        //Create new button element
+        var button = document.createElement("button");
 
-                };
-            }(blocklist[i]);
-            // button.addEventListener('click',function(){
-            //     alert(button.innerHTML);
-            //     chrome.runtime.sendMessage({deleteEntry:button.innerHTML});
-            // })
+        //Set the button's text
+        button.innerHTML = blocklist[i].url;
+        button.id = blocklist[i].id;
+        console.log(blocklist[i]);
+        button.className = "button";
+        button.onclick = (function () {
+          return function () {
+            //on click, send message to background.js to delete site from blocklist + remove button interface from HTML container
+            chrome.runtime.sendMessage({ deleteEntry: this.id });
+            buttonContainer.removeChild(this);
+          };
+        })(blocklist[i]);
+        // button.addEventListener('click',function(){
+        //     alert(button.innerHTML);
+        //     chrome.runtime.sendMessage({deleteEntry:button.innerHTML});
+        // })
 
-            //Append the button to the button container
-            buttonContainer.appendChild(button);
-        }
+        //Append the button to the button container
+        buttonContainer.appendChild(button);
+      }
     }
+  }
 });
